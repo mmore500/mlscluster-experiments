d579246b02f05d8175fe48b9e45ba05abe009781
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-02 13:08:19 -0400

    Clean up/consolidate app.R

--- R/app.R
+++ R/app.R
@@ -5,51 +5,15 @@ invisible(lapply(libs_load, library, character.only = TRUE))
 library(parallel)
 NCPU <- detectCores()
 if (is.na(NCPU)) {
-  NCPU <- 7  # Fallback if detection fails
+  NCPU <- 7 # Fallback if detection fails
 }
-PERIOD_INTEREST <- 2  # somehow needed in stats_multiple_thresholds, wtf
-THRESHOLD_INTEREST <- 2
 options(scipen = 999)
 
-thr <- c(0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 10, 25)
-quantl <- c(
-  1 / 400,
-  1 / 200,
-  1 / 400,
-  1 / 100,
-  2 / 100,
-  3 / 100,
-  4 / 100,
-  5 / 100,
-  1 / 10,
-  1 / 4
-)
-pal_lineages <- c(
-  B.1.1.7 = "#fd7f6f",
-  `AY.4.*` = "#7eb0d5",
-  `AY.* (non-AY.4.*)` = "#b2e061",
-  `B.1.177.*` = "#bd7ebe",
-  `BA.1.*` = "#ffb55a",
-  `BA.2.*` = "#ffcc00",
-  Other = "#fdcce5"
-)
-thr_pref <- "threshold_quantile"
-path_thresholds <- c(
-  glue::glue("{thr_pref}_0.25"),
-  glue::glue("{thr_pref}_0.5"),
-  glue::glue("{thr_pref}_0.75"),
-  glue::glue("{thr_pref}_1"),
-  glue::glue("{thr_pref}_2"),
-  glue::glue("{thr_pref}_3"),
-  glue::glue("{thr_pref}_4"),
-  glue::glue("{thr_pref}_5"),
-  glue::glue("{thr_pref}_10"),
-  glue::glue("{thr_pref}_25")
-)
+###############################################################################
+## Run mlsclust
+###############################################################################
 
-# Load metadata and tree sc2_md_curated <-
-# readRDS('rds/sc2_md_curated_WITH_Xs_Ns.rds')
-# sc2_tre_curated <- readRDS('rds/sc2_tre_curated.rds')
+# Load metadata and tree
 sc2_md_curated <- readRDS(url(
   "https://osf.io/f953r/download",
   "rb"
@@ -59,11 +23,8 @@ sc2_tre_curated <- readRDS(url(
   "rb"
 ))
 
-# options(max.print=999999)
+# ----------------------------------------------------------------------------
 
-## 1. Run mlsclust ## Period 1: testing (without artifact
-## removal: ~10 minutes: 6 for mlsclust and 4 for
-## run_diff_thresholds)
 start <- Sys.time()
 res_p1 <- mlsclust(
   sc2_tre_curated,
@@ -80,14 +41,48 @@ res_p1 <- mlsclust(
   root_on_tip_sample_time = 2019.995,
   detailed_output = FALSE,
   ncpu = NCPU
-) # detailed_output=TRUE
-end <- Sys.time()
-total_time_p1 <- as.numeric(end - start, units = "mins")
-message(paste("Total time elapsed:", total_time_p1, "mins"))
+)
+message(paste(
+  "Total time elapsed:",
+  as.numeric(Sys.time() - start, units = "mins"),
+  "mins"
+))
 system(glue("mkdir -p rds/"))
 saveRDS(res_p1, "rds/res_p1.rds")
 
 # res_p1 <- readRDS('rds/res_p1.rds')
+
+###############################################################################
+## Thresholds
+###############################################################################
+thr <- c(0.25, 0.5, 0.75, 1, 2, 3, 4, 5, 10, 25)
+quantl <- c(
+  1 / 400,
+  1 / 200,
+  1 / 400,
+  1 / 100,
+  2 / 100,
+  3 / 100,
+  4 / 100,
+  5 / 100,
+  1 / 10,
+  1 / 4
+)
+thr_pref <- "threshold_quantile"
+path_thresholds <- c(
+  glue::glue("{thr_pref}_0.25"),
+  glue::glue("{thr_pref}_0.5"),
+  glue::glue("{thr_pref}_0.75"),
+  glue::glue("{thr_pref}_1"),
+  glue::glue("{thr_pref}_2"),
+  glue::glue("{thr_pref}_3"),
+  glue::glue("{thr_pref}_4"),
+  glue::glue("{thr_pref}_5"),
+  glue::glue("{thr_pref}_10"),
+  glue::glue("{thr_pref}_25")
+)
+
+# ----------------------------------------------------------------------------
 
 start <- Sys.time()
 for (i in 1:length(thr)) {
@@ -106,31 +101,39 @@ for (i in 1:length(thr)) {
     compute_tree_annots = FALSE,
     plot_global_mut_freq = FALSE,
     detailed_output = FALSE
-  ) # , desc_sisters=list(res_p1[[4]], res_p1[[5]]), amd=res_p1[[6]]
+  )
 }
-end <- Sys.time()
-total_time_p1_t <- as.numeric(end - start, units = "mins")
 message(paste(
   "Total time elapsed:",
-  total_time_p1_t,
+  as.numeric(Sys.time() - start, units = "mins"),
   "mins"
 ))
 
-## 2. Run statistical tests for multiple thresholds ##
+###############################################################################
+## Run statistical tests for multiple thresholds
+###############################################################################
+
+# somehow needed in stats_multiple_thresholds, wtf
+major_lineages <- c(
+  "B.1.177.*",
+  "B.1.1.7",
+  "AY.4.*",
+  "AY.* (non-AY.4.*)",
+  "Other"
+)
+pal_lineages <- c(
+  B.1.1.7 = "#fd7f6f",
+  `AY.4.*` = "#7eb0d5",
+  `AY.* (non-AY.4.*)` = "#b2e061",
+  `B.1.177.*` = "#bd7ebe",
+  `BA.1.*` = "#ffb55a",
+  `BA.2.*` = "#ffcc00",
+  Other = "#fdcce5"
+)
+
 # Expected paths containing results for each quantile threshold
 table_names_periods <- c(2)
-table_names_thresholds <- c(
-  0.25,
-  0.5,
-  0.75,
-  1,
-  2,
-  3,
-  4,
-  5,
-  10,
-  25
-)
+table_names_thresholds <- thr
 table_combs <- do.call(
   paste,
   c(
@@ -141,42 +144,40 @@ table_combs <- do.call(
     sep = "_"
   )
 )
-out_folder <- "period1"
+
+# ----------------------------------------------------------------------------
 
 system(glue("mkdir -p stat_results/plots_paper/"))
 # Override pal_lineages from package with new major_lineage labels
-major_lineages <- c(  # somehow needed in stats_multiple_thresholds, wtf
-  "B.1.177.*",
-  "B.1.1.7",
-  "AY.4.*",
-  "AY.* (non-AY.4.*)",
-  "Other"
-) # 'Beta_B.1.351','Gamma_P.1'
+PERIOD_INTEREST <- 2 # somehow needed in stats_multiple_thresholds, wtf
 rmult_p2 <- stats_multiple_thresholds(
   "results/period1",
   pal_lineages,
   "period1",
   "ED_FileS1.txt"
 )
-plot_tbc_stats("results/period1", out_folder)
+plot_tbc_stats("results/period1", "period1")
 stacked_nsites_genomic_region_mult_thresholds(
   "stat_results/period1/genomewide_plot_non-syn/"
 )
 
-## 4. Run for the selected threshold (2% since FDR ~10% and epsilon ~2.5%)
-# Expected paths containing results for each quantile threshold
-out_folder <- "period1_thr2"
+
+###############################################################################
+## Run for the selected threshold (2% since FDR ~10% and epsilon ~2.5%)
+###############################################################################
+
+THRESHOLD_INTEREST <- 2
+
+# ----------------------------------------------------------------------------
 
 r2 <- stats_selected_threshold(
   "results/period1",
   thr_index = 5,
   pal_lineages,
-  out_folder
+  "period1_thr2"
 ) # NOTE: thr_index = 5 -> 2%
-# Extended data fig. S5: mlscluster, intersection, and
-# hyphy (also stratified by lineage) identified sites for
-# thr=2% and period before Omicron
-r2[[4]]
+# Extended data fig. S5: mlscluster, intersection, and hyphy (also stratified
+# by lineage) identified sites for thr=2%
 ggsave(
   glue("stat_results/plots_paper/ED_FigS5.png"),
   units = "px",
