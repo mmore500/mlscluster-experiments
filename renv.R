9e5d4ab3c7a69612b9f9d47c907fd5426c935ae1
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2025-04-03 01:46:22 -0400

    Bump mlscluster pin for bugfixes

--- renv.R
+++ renv.R
@@ -128,7 +128,7 @@ if (!requireNamespace("devtools", quietly = TRUE)) {
 
 # Install the GitHub package from mrc-ide
 devtools::install_github("YuLab-SMU/ggtree@c17773c973d6c4036ee3af40a3957fb74d8ee9ff")
-devtools::install_github('mmore500/mlscluster@aab4bce621c0abf67f341c3dd98f9993bc2326e2')
+devtools::install_github('mmore500/mlscluster@ddddeeb0e26c26c6f5371de16b06b8e4c88c26a4')
 
 # Snapshot the current state of the library to record package versions in renv.lock
 renv::snapshot(packages = cran_packages)
